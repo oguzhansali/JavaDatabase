110cfde103d0e28a986857ddfaa332c6f750a154
oguzhansali61 <154914330+oguzhansali61@users.noreply.github.com>
2024-06-03 15:16:45 +0300

    .

--- src/DBConn.java
+++ src/DBConn.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class DBConn {
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConn {
+    public static final String DB_URL="jdbc:postgresql://localhost:5432/University";
+    public static final String DB_USER="postgres";
+    public static final String DB_PASSWORD="40427034004";
+
+    public static void main(String[] args) throws SQLException {
+        try{
+            Connection connect = null;
+            connect= DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
